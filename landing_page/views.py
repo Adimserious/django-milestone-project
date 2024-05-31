4bb36acff8bee151aac941b42e81280bc5f9374c
Adimserious Amatobi <lilianadimchinobi@gmail.com>
2024-05-31 10:06:46 +0000

    Adjust landing_page views.py files

--- landing_page/views.py
+++ landing_page/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    if request.method == "GET":
-        return HttpResponse("Great job Lilian; this was a get request")
-    else request.method == "POST":
-        return HttpResponse("This was a post request")
+    if request.method == "POST":
+        return HttpResponse("Great job; this was a post request")
+    else:
+        return HttpResponse(request.method)
